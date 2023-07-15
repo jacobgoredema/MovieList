36dab1d0510120ad0a74adf995b8a1addb6659d9
jacobgoredema <jacobgoredema@gmail.com>
2023-07-15 19:30:32 +0200

    Add project files.

---
+++ MovieList/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MovieList.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext dbContext { get; set; }
+
+        public HomeController(MovieContext movieContext, ILogger<HomeController> logger)
+        {
+            dbContext = movieContext;
+            _logger = logger;
+
+        }
+
+        public IActionResult Index()
+        {
+            var movies = dbContext.Movies.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+
+            return View(movies);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+
+
+
---
+++ MovieList/Controllers/MovieController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieList.Controllers
+{
+    public class MovieController : Controller
+    {
+        private MovieContext dbContext { get; set; }
+
+        public MovieController(MovieContext context)
+        {
+            dbContext = context;
+
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Genres = dbContext.Genres.OrderBy(g => g.Name).ToList();
+
+            return View("Edit", new Movie());
+
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Genres = dbContext.Genres.OrderBy(g => g.Name).ToList();
+            var movie = dbContext.Movies.Find(id);
+
+            return View(movie);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                if (movie.MovieId == 0)
+                    dbContext.Movies.Add(movie);
+                else
+                    dbContext.Movies.Update(movie);
+
+                return RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                ViewBag.Action = (movie.MovieId == 0) ? "Add" : "Edit";
+                ViewBag.Genres = dbContext.Genres.OrderBy(g => g.Name).ToList();
+
+                return View(movie);
+
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var movie = dbContext.Movies.Find(id);
+
+            return View(movie);
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Movie movie)
+        {
+            dbContext.Movies.Remove(movie);
+            dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+    }
+}
